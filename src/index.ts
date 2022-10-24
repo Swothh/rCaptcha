971dbe523b30d250bfbac6e14cea779788a98b29
Loiren <84875764+Loirenn@users.noreply.github.com>
2022-10-24 18:34:21 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import { IrCaptcha } from "./interfaces";
 import Canvas from 'canvas';
 
-export class rCaptcha implements IrCaptcha {
+export class Captcha implements IrCaptcha {
     public difficulty: IrCaptcha['difficulty'];
     public length: IrCaptcha['length'];
     public captcha?: IrCaptcha['captcha'];
+    public keywords?: IrCaptcha['keywords'];
     constructor(rCaptcha: IrCaptcha) {
         this.difficulty = rCaptcha.difficulty;
         this.length = rCaptcha.length;
         this.captcha = rCaptcha.captcha;
+        this.keywords = rCaptcha.keywords;
     };
 
     public async generate() {
         if (this.length < 5) throw new Error('Length must be greater than 5.');
         if (this.length > 10) throw new Error('Length must be less than 10');
+        if (this.keywords && this.keywords.length < 5) throw new Error('Keywords must be greater than 5.');
 
-        let captcha = generateCaptcha(this.difficulty, this.length);
+        let captcha = generateCaptcha(this.difficulty, this.length, this.keywords);
 
         if (this.captcha?.backgroundImage) {
             let canvas = Canvas.createCanvas(900, 300);
@@ -71,41 +74,50 @@ export class rCaptcha implements IrCaptcha {
     };
 };
 
-function generateCaptcha(difficulty: 'KOLAY' | 'ORTA' | 'ZOR' | 'ÇOKZOR' | 'EASY' | 'MEDIUM' | 'HARD' | 'VERYHARD', length: number): string {
+function generateCaptcha(difficulty: 'KOLAY' | 'ORTA' | 'ZOR' | 'ÇOKZOR' | 'EASY' | 'MEDIUM' | 'HARD' | 'VERYHARD', length: number, isCustomKeywords?: string): string {
     let keywords;
-    switch (difficulty) {
-        case 'KOLAY':
-            keywords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'ORTA':
-            keywords = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'ZOR':
-            keywords = "01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'ÇOKZOR':
-            keywords = "!+#>-<@&%01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'EASY':
-            keywords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'MEDIUM':
-            keywords = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'HARD':
-            keywords = "01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        case 'VERYHARD':
-            keywords = "!+#>-<@&%01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-        default:
-            keywords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            break;
-    };
-
-    let captcha = '';
-    for (let i = 0; i < length; i++) {
-        captcha += keywords.charAt(Math.floor(Math.random() * keywords.length));
-    };
-    return captcha;
+    if (isCustomKeywords) {
+        keywords = isCustomKeywords;
+        let captcha = '';
+        for (let i = 0; i < length; i++) {
+            captcha += keywords.charAt(Math.floor(Math.random() * keywords.length));
+        };
+        return captcha;
+    } else {
+        switch (difficulty) {
+            case 'KOLAY':
+                keywords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'ORTA':
+                keywords = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'ZOR':
+                keywords = "01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'ÇOKZOR':
+                keywords = "!+#>-<@&%01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'EASY':
+                keywords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'MEDIUM':
+                keywords = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'HARD':
+                keywords = "01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            case 'VERYHARD':
+                keywords = "!+#>-<@&%01234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+            default:
+                keywords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+        };
+    
+        let captcha = '';
+        for (let i = 0; i < length; i++) {
+            captcha += keywords.charAt(Math.floor(Math.random() * keywords.length));
+        };
+        return captcha;
+    }
 };
