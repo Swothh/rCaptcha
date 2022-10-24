1efb8a6e4450732689276861702ca0f51b5af5d0
Loiren <84875764+Loirenn@users.noreply.github.com>
2022-10-24 18:34:40 +0300

    Update rCaptcha.ts

--- src/interfaces/rCaptcha.ts
+++ src/interfaces/rCaptcha.ts
@@ -3,6 +3,7 @@ import { Difficulty } from "src/utils";
 export interface IrCaptcha {
     difficulty: 'KOLAY' | 'ORTA' | 'ZOR' | 'ÇOKZOR' | 'EASY' | 'MEDIUM' | 'HARD' | 'VERYHARD';
     length: number;
+    keywords?: string;
     captcha?: {
         backgroundColor?: string;
         textColor?: string;
